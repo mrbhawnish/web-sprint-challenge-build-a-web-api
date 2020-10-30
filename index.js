446fa780a8d80911caf122e1bf99e7afb4afc7c0
bhawnishk1 <mrbhawnish@gmail.com>
2020-10-30 16:35:24 -0700

    PORT added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const PORT = 3000
+require('dotenv').config();
 const server = require('./api/server');
+const PORT = process.env.PORT || 3000
 
 server.listen(PORT, (req, res) => {
-    console.log('API is up and running at' + PORT)
+    console.log('API is up and running at ' + PORT)
 });
